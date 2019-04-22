aade4d20e3476264114e305c87bc4a432aaa55b8
priya suresh <priyasuresh@MacBook-Pro-5.local>
2019-04-22 14:36:12 -0700

    gaming complete

---
+++ server/index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
